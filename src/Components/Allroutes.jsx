a2ca74f55849a9c6789f1737e4e90a91a20ad687
Subhasmita Mohapatra <136898282+Neha9178@users.noreply.github.com>
2023-07-28 20:55:28 +0530

    Update Allroutes.jsx

--- src/Components/Allroutes.jsx
+++ src/Components/Allroutes.jsx
@@ -5,23 +5,24 @@ import AddNewAddress from "../Pages/AddNewAddress";
 import BillingDetails from "../Components/BillingDetails";
 import Payment from "../Pages/Payment";
 import AddressDetails from "../Components/AddressDetails";
-import CartPage from "../Pages/CartPage";
+import OrderPlaced from "../Pages/OrderPlaced";
 
 const Allroutes = () => {
   return (
     <div className="App">
       <Routes>
+        <Route path="/App" element={<App />} />
         <Route path="/cartpage" element={<CartPage />} />
+        <Route path="/addnewaddress" element={<AddNewAddress />} />
         <Route path="/address" element={<PopupAddressForm />} />
         <Route path="/addressdetails" element={<AddressDetails />} />
-        <Route path="/addnewaddress" element={<AddNewAddress />} />
         <Route path="/billingdetails" element={<BillingDetails />} />
         <Route path="/payment" element={<Payment />} />
+        <Route path="/orderplaced" element={<OrderPlaced />} />
       </Routes>
     </div>
   );
 };
 
 export default Allroutes;
-
 
